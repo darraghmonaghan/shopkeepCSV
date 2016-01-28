4f48e19bf0f42200f3881e4a4e20bc4f112d8670
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-01-28 14:23:05 +0000

    refactored modifiers component

--- shopkeepTest.rb
+++ shopkeepTest.rb
@@ -7,14 +7,11 @@ puts JSON.pretty_generate(CSV.open('./example.csv', headers: true).map do |row|
   row.each do |k, v|
   end
   
+  modifiers =   	
+
   	if row['modifier_1_name'] && row['modifier_2_name'] && row['modifier_3_name']
-		  { id: row['item id'].to_i,
-		  	description: row['description'],
-		  	price: row['price'].scan(/[.0-9]/).join().to_f,
-		  	cost: row['cost'],
-		  	price_type: row['price_type'],
-		  	quantity_on_hand: row['quantity_on_hand'].to_i,
-			modifiers: [
+
+				[
 				  	{ name: row['modifier_1_name'],
 				  	  price: row['modifier_1_price'].scan(/[.0-9]/).join().to_f
 				  	},
@@ -22,20 +19,15 @@ puts JSON.pretty_generate(CSV.open('./example.csv', headers: true).map do |row|
 				  	  name: row['modifier_2_name'],
   					  price: row['modifier_2_price'].scan(/[.0-9]/).join().to_f				  		
 				  	},
-				  	{ name: row['modifier_3_name'],
+				  	{ 
+				  	  name: row['modifier_3_name'],
 				  	  price: row['modifier_3_price'].scan(/[.0-9]/).join().to_f
-				  	},
+				  	}
 			   ]
-		  }
 
   	elsif row['modifier_1_name'] && row['modifier_2_name']
-		  { id: row['item id'].to_i,
-		  	description: row['description'],
-		  	price: row['price'].scan(/[.0-9]/).join().to_f,
-		  	cost: row['cost'],
-		  	price_type: row['price_type'],
-		  	quantity_on_hand: row['quantity_on_hand'].to_i,
-			modifiers: [
+
+				[
 				  	{ name: row['modifier_1_name'],
 				  	  price: row['modifier_1_price'].scan(/[.0-9]/).join().to_f
 				  	},
@@ -44,50 +36,33 @@ puts JSON.pretty_generate(CSV.open('./example.csv', headers: true).map do |row|
   					  price: row['modifier_2_price'].scan(/[.0-9]/).join().to_f				  		
 				  	}
 			   ]
-		  }
 
     elsif row['modifier_1_name']
-		  { id: row['item id'].to_i,
-		  	description: row['description'],
-		  	price: row['price'].scan(/[.0-9]/).join().to_f,
-		  	cost: row['cost'],
-		  	price_type: row['price_type'],
-		  	quantity_on_hand: row['quantity_on_hand'].to_i,
-			modifiers: [
-				  	  	name: row['modifier_1_name'],
-				  		price: row['modifier_1_price'].scan(/[.0-9]/).join().to_f
-			]
-		  }
 
-	else 
-		  { id: row['item id'].to_i,
-		  	description: row['description'],
-		  	price: row['price'].scan(/[.0-9]/).join().to_f,
-		  	cost: row['cost'],
-		  	price_type: row['price_type'],
-		  	quantity_on_hand: row['quantity_on_hand'].to_i
-		  }
+				[
+				  	name: row['modifier_1_name'],
+				  	price: row['modifier_1_price'].scan(/[.0-9]/).join().to_f
+				]
+
+	else
+		[]
 	end
+
+
+  	{ id: row['item id'].to_i,
+	  description: row['description'],
+      price: row['price'],
+	  cost: row['cost'],
+	  price_type: row['price_type'],
+      quantity_on_hand: row['quantity_on_hand'].to_i,
+      modifiers: modifiers
+    }
+
 
 
 end)
 
 
-
-
-
-
-  	# puts row['modifier_1_price'].scan(/[.0-9]/).join().to_f
-  	# puts row['modifier_1_price'].class
-
-
-
-  	  		# ,{
-  		# 	name: row['modifier_2_name'],
-  		# 	price: row['modifier_2_price']
-  		# },{
-  		# 	name: row['modifier_3_name'],
-  		# 	price: row['modifier_3_price']
-  		# },
+# .scan(/[.0-9]/).join().to_f
 
 
