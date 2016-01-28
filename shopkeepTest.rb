5598e06bdd63ae1287a312e04a57809f22e52480
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-01-28 18:02:00 +0000

    refactored around functions

--- shopkeepTest.rb
+++ shopkeepTest.rb
@@ -2,88 +2,102 @@ require 'csv'
 require 'json'
 
 
-export_file = './exportedData.json'
+export_file = './exportedData.json'					## variable of filename JSON to be exported to
+
+
+#######################         Supporting Functions        ##########################
+
+		def currency_to_integer(string)
+			return string.scan(/[-.0-9]/).join().to_f
+		end
+
+		def string_to_integer(string)
+			return string.to_i
+		end
+
+		def modifier_object(name1, price1, name2, price2, name3, price3)
+
+			  ### Ternary Operators to address blanks / null entries ###
+			  modifier_1_price = price1 != nil ? currency_to_integer(price1) : nil
+
+			  modifier_2_price = price2 != nil ? currency_to_integer(price2) : nil
+			  
+			  modifier_3_price = price3 != nil ? currency_to_integer(price3) : nil
+
+			  	if name1 && name2 && name3
+					return [
+							  	{ name: name1,
+							  	  price: modifier_1_price				
+							  	},
+							  	{
+							  	  name: name2,
+			  					  price: modifier_2_price				  		
+							  	},
+							  	{ 
+							  	  name: name3,
+			  					  price: modifier_3_price	
+							  	}
+						   ]
+
+			  	elsif name1 && name2
+					return [
+							  	{ name: name1,
+							  	  price: modifier_1_price
+							  	},
+							  	{
+							  	  name: name2,
+			  					  price: modifier_2_price				  		
+							  	}
+						   ]
+
+			    elsif name1
+					return [
+							  	{ name: name1,
+							  	  price: modifier_1_price
+							  	}
+							]
+
+				else
+					[]
+				end
+		end
+
+
+
+#######################         CSV to JSON function        ##########################
+
 
 
 output = JSON.pretty_generate(CSV.open('./example.csv', headers: true).map do |row|
-  # modifier = {}
-  row.each do |k, v|
+  
+
+  row.each do |k, v|						### WHAT IS THIS??????
   end
+  											## and what is this end doing? necessary?
+
+
+  ### Creating variables to pass to JSON object ###
+  quantity_on_hand = row['quantity_on_hand'] != nil ? string_to_integer(row['quantity_on_hand']) : nil
   
-
-  ## 1.   Ternary Operators to address blanks / null ##
-
-  modifier_1_name = row['modifier_1_name']
-  modifier_1_price = row['modifier_1_price'] != nil ? row['modifier_1_price'].scan(/[-.0-9]/).join().to_f : nil
-
-  modifier_2_name = row['modifier_2_name']
-  modifier_2_price = row['modifier_2_price'] != nil ? row['modifier_2_price'].scan(/[-.0-9]/).join().to_f : nil
+  price = row['price'] != nil ? currency_to_integer(row['price']) : nil
   
-  modifier_3_name = row['modifier_3_name']
-  modifier_3_price = row['modifier_3_price'] != nil ? row['modifier_3_price'].scan(/[-.0-9]/).join().to_f : nil
+  cost = row['cost'] != nil ? currency_to_integer(row['cost']) : nil
 
-  quantity_on_hand = row['quantity_on_hand'] != nil ? row['quantity_on_hand'].to_i : nil
-  price = row['price'] != nil ? row['price'].scan(/[-.0-9]/).join().to_f : nil
-  cost = row['cost'] != nil ? row['cost'].scan(/[-.0-9]/).join().to_f : nil
+  id = string_to_integer(row['item id'])
 
 
-  ## 2.   Ensuring Price_Type is accurate - only nil / 0.0 price items can be "open"  ##
-
-  # if price != nil || 0.0
-  # 	price_type = 'system'
-  # else
-  # 	price_type = 'open'
-  # end
-
-
-  ## 3.   Creating modifiers object, passed into hash later ##
-
-  modifiers =   	
-
-  	if row['modifier_1_name'] && row['modifier_2_name'] && row['modifier_3_name']
-
-				[
-				  	{ name: modifier_1_name,
-				  	  price: modifier_1_price
-				  	},
-				  	{
-				  	  name: modifier_2_name,
-  					  price: modifier_2_price				  		
-				  	},
-				  	{ 
-				  	  name: modifier_3_name,
-  					  price: modifier_3_price	
-				  	}
-			   ]
-
-  	elsif row['modifier_1_name'] && row['modifier_2_name']
-
-				[
-				  	{ name: modifier_1_name,
-				  	  price: modifier_1_price
-				  	},
-				  	{
-				  	  name: modifier_2_name,
-  					  price: modifier_2_price				  		
-				  	}
-			   ]
-
-    elsif row['modifier_1_name']
-
-				[
-				  	{ name: modifier_1_name,
-				  	  price: modifier_1_price
-				  	}
-				]
-
-	else
-		[]
-	end
+  ### Creating modifiers object as a variable ###
+  modifiers = modifier_object(row['modifier_1_name'],
+  							  row['modifier_1_price'],
+  							  row['modifier_2_name'],
+  							  row['modifier_2_price'], 
+  							  row['modifier_3_name'],
+  							  row['modifier_3_price'])	
 
 
-  ## 4.   Creating each object entered into JSON  ##
+  ### Creating each object entered into JSON file  ###
 
-  	{ id: row['item id'].to_i,
+  	{ id: id,
 	  description: row['description'],
       price: price,								## variable passed from ternary operator value
 	  cost: cost,								## variable passed from ternary operator value
@@ -95,6 +109,19 @@ output = JSON.pretty_generate(CSV.open('./example.csv', headers: true).map do |r
 
 
 end)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 puts output
 
