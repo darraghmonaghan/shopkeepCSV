f5ad64df8b9995ccaee6c943784360c4dbd3286f
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-01-31 19:43:58 +0000

    reformatting

--- importer_exporter.rb
+++ importer_exporter.rb
@@ -2,18 +2,33 @@ require 'csv'                       							## do not touch
 require 'json'                      							## do not touch
 require './functions/support_functions/check_input_data.rb'   	## do not touch
 
+##############################################################################################
+#################################       Instructions         ################################# 
+##############################################################################################
 
-########################       Variables required to start conversion     #################### 
-
-conversion_type = 'product'                     ##   OPTIONS: 'Product' (Eventually "Customer" and "Transaction" will be added)
-import_file = './target_import_file.csv'                   ##   1) Ensure target docutment exists, 2) Ensure correct path
-export_file = './target_export_file.json'				##   variable of filename to be exported to - ensure correct path
-input_format = 'CSV'                            ##   OPTIONS:  'CSV'    (Eventually 'XML', 'JSON')
-output_format = 'JSON'                          ##   OPTIONS:  'JSON'   (Eventually 'XML', 'CSV')
+### Conversion Type           Options: 'Product' (Eventually "Customer" and "Transaction" will be added)
+### Import File               Enter target import file path name - Ensure target document exists
+### Export File               Enter target export file path name - Ensure target document exists
+### Input Format              Enter format of target import file - Options: 'CSV'  (Eventually 'XML', 'JSON')
+### Export Format             Enter format of target export file - Options: 'JSON' (Eventually 'CSV', 'XML')
 
 
 
-#######################         Initiate Conversion  - DO NOT TOUCH      ##########################
+##############################################################################################
+########################       Variables Required for Conversion     ######################### 
+##############################################################################################
+
+conversion_type = 'Product'                     
+import_file = './target_import_file.csv'        
+export_file = './target_export_file.json'	
+input_format = 'CSV'                            
+output_format = 'JSON'                          
+
+
+
+##############################################################################################
+###############################          Initiate Conversion         ######################### 
+##############################################################################################
 
 check_input_data(conversion_type, import_file, export_file, input_format, output_format)
 
